c88b7ba261070b19cdc6245b7cf0eb274c8c86ef
unknown <developer.m.irfan@gmail.com>
2022-08-28 00:36:37 -0700

    first verison

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import HomeSec from './components/HomeSection/HomeSec'
+import Navbar from './components/Navbar/Navbar'
+
+function Main() {
+  return (
+    <div>
+        <Navbar />
+        <HomeSec />
+    </div>
+  )
+}
+
+export default Main
